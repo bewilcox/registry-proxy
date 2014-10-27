dc9ea672936d508330de5dd54270144c970ba03a
bwilcox <wilcox.benoit@gmail.com>
2014-10-27 23:31:03 +0100

    Test with Yoke

--- src/main/java/org/dockhouse/registryproxy/verticle/ProxyServer.java
+++ src/main/java/org/dockhouse/registryproxy/verticle/ProxyServer.java
@@ -1,8 +1,9 @@
 package org.dockhouse.registryproxy.verticle;
 
-import org.dockhouse.registryproxy.communication.CommunicationUtils;
-import org.vertx.java.core.http.HttpClient;
-import org.vertx.java.core.http.HttpClientRequest;
+import com.jetdrone.vertx.yoke.Yoke;
+import com.jetdrone.vertx.yoke.middleware.RequestProxy;
+import com.jetdrone.vertx.yoke.middleware.Router;
+import org.vertx.java.core.Handler;
 import org.vertx.java.platform.Verticle;
 
 /**
@@ -15,49 +16,34 @@ public class ProxyServer extends Verticle {
     @Override
     public void start() {
 
-        // Private registry Host
-        final String registryHost = this.container.env().getOrDefault(CommunicationUtils.PRIVATE_REGISTRY_HOST, "localhost");
-        final String registryPort = this.container.env().getOrDefault(CommunicationUtils.PRIVATE_REGISTRY_PORT, "5000");
+        Yoke yoke = new Yoke(vertx);
 
-        // Init the Http client for the private registry Docker
-        this.container.logger().info("Connection initialisation to the Docker private registry {" + registryHost+":"+registryPort+"}");
-        final HttpClient registryHttpClient = vertx.createHttpClient().setHost(registryHost).setPort(Integer.parseInt(registryPort));
+        yoke.use(new Router()
+            .all("^/", req -> {
+                System.out.println("handle request " + req.normalizedPath());
+                RequestProxy requestProxy = new RequestProxy("localhost", 9999, false);
 
-        // Init the Proxy server
-        this.container.logger().info("Initialisation of the proxy server");
-        vertx.createHttpServer().requestHandler(requestReceiveByProxy -> {
-            this.container.logger().info("Proxing request : " + requestReceiveByProxy.uri());
-
-            // Pass the request to the private registry and process the response
-            final HttpClientRequest requestForRegistry = registryHttpClient.request(requestReceiveByProxy.method(), requestReceiveByProxy.uri(), responseFromRegistry -> {
-                this.container.logger().info("Processing response " + responseFromRegistry.statusCode());
-                requestReceiveByProxy.response().setStatusCode(responseFromRegistry.statusCode());
-                requestReceiveByProxy.response().headers().set(responseFromRegistry.headers());
-                requestReceiveByProxy.response().setChunked(true);
-
-                // Pass the registry datas to the client
-                responseFromRegistry.dataHandler(data -> {
-                    this.container.logger().info("Proxying response body : " + data);
-                    requestReceiveByProxy.response().write(data);
+                requestProxy.handle(req, new Handler<Object>() {
+                    @Override
+                    public void handle(Object o) {
+                        System.out.println("handle Object from RequestProxy");
+                        if (o != null) {
+                            System.out.printf(o.toString());
+                            o.toString();
+                        }
+                    }
                 });
+            })
+        );
 
-                // Flag the end of the registry response
-                responseFromRegistry.endHandler(aVoid -> requestReceiveByProxy.response().end());
-            });
+        yoke.listen(5001);
 
-            // Process the request received by the proxy server and process the request for the private registry
-            requestForRegistry.headers().set(requestReceiveByProxy.headers());
-            requestForRegistry.setChunked(true);
-            requestReceiveByProxy.dataHandler( data ->  {
-                this.container.logger().info("Proxing request body" + data);
-                requestForRegistry.write(data);
-            });
-            requestReceiveByProxy.endHandler(aVoid -> {
-                this.container.logger().info("end of the request received by the proxy");
-                requestForRegistry.end();
-            });
+        Yoke yoke2 = new Yoke(vertx);
+        yoke2.use(new Router().all("/", req -> {
+            System.out.println("Client receive proxing res");
+            req.response().end("This is the response");
+        })).listen(9999);
 
-        }).listen(5001);
 
     }
 }
